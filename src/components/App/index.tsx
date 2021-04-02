5aba4c600f207ccee6b9b16b870b5df70c1b9c77
Hanna Nyman <hanna.nyman@veridict.com>
2021-04-02 16:30:45 +0200

    Some cleanup

---
+++ src/components/App/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+const App = () => {
+  return (
+    <div className="App">
+      <p>Hejsan från App</p>
+    </div>
+  )
+}
+
+export default App;
