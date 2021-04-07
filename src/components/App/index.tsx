de2b5204e38d302cd8e4498a98e16f37594506ba
Hanna Nyman <hanna.nyman@veridict.com>
2021-04-07 16:12:36 +0200

    More game logic and events

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import NumberDisplay from '../NumberDisplay';
-import { generateCells } from '../../utils';
+import { generateCells, openMultipleCells } from '../../utils';
 import Button from '../Button';
-import { CellState, Face } from '../../types';
+import { CellState, CellValue, Face } from '../../types';
 import './App.scss';
 
 const App: React.FC = () => {
@@ -47,6 +47,23 @@ const App: React.FC = () => {
   ) => () => {
     if (!live) {
       setLive(true)
+    }
+
+    const currentCell = cells[rowParam][colParam];
+    let newCells = cells.slice();
+
+    if ([CellState.flagged, CellState.visible].includes(currentCell.state)) {
+      return;
+    }
+
+    if (currentCell.value === CellValue.bomb) {
+
+    } else if (currentCell.value === CellValue.none) {
+      newCells = openMultipleCells(newCells, rowParam, colParam);
+      setCells(newCells);
+    } else {
+      newCells[rowParam][colParam].state = CellState.visible;
+      setCells(newCells);
     }
   }
 
